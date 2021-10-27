e8a31e35ec3c3f672c843d79f96cd83ed1a54a20
unknown <joelpimentel1995@gmail.com>
2021-10-27 08:02:56 -0500

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FormTask from './components/FormTask';
+import Navbar from './components/Navbar';
 
-function App() {
+interface Props {
+  title?: string;
+}
+
+function App({ title }: Props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div>
+     <Navbar />
+     <div className="container">
+     <h1>{title}</h1>
+      <FormTask />
+     </div>
     </div>
   );
 }
