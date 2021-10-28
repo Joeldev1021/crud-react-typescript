33a3285347df9664b002d3373cb51969a34c495a
unknown <joelpimentel1995@gmail.com>
2021-10-27 19:31:11 -0500

    add task ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import React from 'react';
-import './App.css';
-import FormTask from './components/FormTask';
-import Navbar from './components/Navbar';
+import React, { ChangeEvent, useState } from "react";
+import "./App.css";
+import CardTask from "./components/CardTask";
+import FormTask from "./components/FormTask";
+import Navbar from "./components/Navbar";
+import { TaskProps } from "./interface/index"
+
 
 interface Props {
-  title?: string;
+  title: string;
 }
 
 function App({ title }: Props) {
+
+const [tasks, setTasks] = useState<TaskProps[]>([]);
+
+
+const addTasks = (task: TaskProps):void => {
+   setTasks([...tasks, {...task}]);
+   
+}
   return (
-   <div>
-     <Navbar />
-     <div className="container">
-     <h1>{title}</h1>
-      <FormTask />
-     </div>
+    <div>
+      <Navbar />
+      <div className="container ">
+        <h1>{title}</h1>
+         <FormTask addTask={addTasks}/>
+          <div className="row row-cols-5">
+          <CardTask tasks={tasks}/>
+         </div>
+        </div>
     </div>
   );
 }
